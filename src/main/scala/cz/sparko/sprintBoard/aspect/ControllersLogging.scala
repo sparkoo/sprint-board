0d75b44577bc4af7d68568b896ad2ad00d9df5d0
Michal Vala <michal.vala@gmail.com>
2015-09-15 07:57:55 +0200

    new lines, logging

---
+++ src/main/scala/cz/sparko/sprintBoard/aspect/ControllersLogging.scala
@@ -0,0 +1,10 @@
+package cz.sparko.sprintBoard.aspect
+
+import org.aspectj.lang.annotation.Aspect
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class ControllersLogging {
+
+}
