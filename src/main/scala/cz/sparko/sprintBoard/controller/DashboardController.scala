190e177f4dbf91420a6eb14f84df5e0e3f005524
Michal Vala <michal.vala@gmail.com>
2015-10-03 10:54:08 +0200

    removed thymeleaf

--- src/main/scala/cz/sparko/sprintBoard/controller/DashboardController.scala
+++
@@ -1,9 +0,0 @@
-package cz.sparko.sprintBoard.controller
-
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
-
-@Controller
-class DashboardController {
-    @RequestMapping(Array("/")) def index() = "home"
-}
